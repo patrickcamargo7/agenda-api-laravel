98a2b13c98ac0b76068cb085e81021ba3b6fd4ff
patrickcamargo7 <patrickcsilv@gmail.com>
2019-05-20 20:35:40 -0300

    Commit Inicial

---
+++ app/Http/Controllers/ContactsController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Contact;
+
+class ContactsController extends Controller
+{
+ /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $contacts = Contact::all();
+
+        return response()->json(compact('contacts'), 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $contact = new Contact();
+        $contact = $contact->fill($data);
+
+        $contact->save();
+
+        return response()->json(compact('contact'), 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $contact = Contact::find($id);
+
+        return response()->json(compact('contact'), 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+
+        $contact = Contact::find($id);
+        $contact = $contact->fill($data);
+
+        $contact->save();
+
+        return response()->json(compact('contact'), 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $contact = Contact::destroy($id);
+
+        return response()->json(['message' => 'removido com sucesso!'], 200);
+    }
+}
