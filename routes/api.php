3fad5c9722e61047db96dffb3e188398d0ed0f2e
patrickcamargo7 <patrickcsilv@gmail.com>
2019-05-20 21:27:04 -0300

    Alterado rota GET de contatos para Resource

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('contacts', 'ContactsController@index');
+Route::resource('contacts', 'ContactsController');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
